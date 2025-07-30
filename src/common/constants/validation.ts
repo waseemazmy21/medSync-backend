4e4214e705c64171cd41a315533f866c767772a1
waseem azmy <waseemazmy21@gmail.com>
2025-07-30 18:29:52 +0300

    Adding Todo in readme

---
+++ src/common/constants/validation.ts
@@ -0,0 +1,12 @@
+export const Validatoin = {
+    user: {
+        email: {
+            required: 'Email is required',
+            invalid: 'Invalid email format',
+        },
+        password: {
+            required: 'Password is required',
+            minlength: 'Password must be at least 6 characters long',
+        },
+    }
+}
